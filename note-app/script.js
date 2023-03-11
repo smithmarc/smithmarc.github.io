f256b74c99208cffd500e945e034b7407b296aca
smithmarc <smivers624@gmail.com>
2023-03-11 22:28:39 +0800

    Update script.js
    New additions from ChatGPT. Will they break things? The magic of Github! I can completely remove them with a single click! I think...

--- note-app/script.js
+++ note-app/script.js
@@ -4,28 +4,59 @@ const titleInput = document.querySelector('#title');
 const contentInput = document.querySelector('#content');
 let noteCount = 0;
 
+// Load notes from localStorage
+const notes = JSON.parse(localStorage.getItem('notes')) || [];
+
+// Render notes to the page
+notes.forEach(noteData => {
+  const note = createNoteElement(noteData);
+  notesContainer.appendChild(note);
+});
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   // Create new note
+  const noteData = {
+    id: Date.now(),
+    title: titleInput.value,
+    content: contentInput.value,
+    timestamp: new Date().toISOString(),
+  };
+
+  const note = createNoteElement(noteData);
+  notesContainer.insertBefore(note, notesContainer.firstChild);
+
+  // Add note data to notes array
+  notes.unshift(noteData);
+
+  // Save notes to localStorage
+  localStorage.setItem('notes', JSON.stringify(notes));
+
+  // Clear form inputs
+  titleInput.value = '';
+  contentInput.value = '';
+});
+
+function createNoteElement(noteData) {
   const note = document.createElement('div');
   note.classList.add('note');
   noteCount++;
 
   const noteTitle = document.createElement('h2');
   noteTitle.classList.add('note-title');
-  noteTitle.innerText = titleInput.value;
+  noteTitle.innerText = noteData.title;
 
   const noteTimestamp = document.createElement('div');
   noteTimestamp.classList.add('note-timestamp');
-  noteTimestamp.innerText = formatDate(new Date());
+  noteTimestamp.innerText = formatDate(new Date(noteData.timestamp));
   noteTitle.appendChild(noteTimestamp);
 
   note.appendChild(noteTitle);
 
   const noteContent = document.createElement('p');
   noteContent.classList.add('note-content');
-  noteContent.innerText = contentInput.value;
+  noteContent.innerText = noteData.content;
   note.appendChild(noteContent);
 
   const deleteButton = document.createElement('button');
@@ -33,24 +64,22 @@ form.addEventListener('submit', (e) => {
   deleteButton.innerHTML = '<i class="icon-trash icon-large"></i>';
   deleteButton.addEventListener('click', () => {
     note.remove();
-    noteCount--;
+
+    // Remove note data from notes array
+    notes = notes.filter(n => n.id !== noteData.id);
+
+    // Save notes to localStorage
+    localStorage.setItem('notes', JSON.stringify(notes));
   });
   note.appendChild(deleteButton);
 
-  // Insert note at the top of other notes
-  notesContainer.insertBefore(note, notesContainer.firstChild);
-
-  // Clear form inputs
-  titleInput.value = '';
-  contentInput.value = '';
-});
+  return note;
+}
 
 function formatDate(date) {
   const hours = date.getHours();
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
-
-  return `${day}/${month}/${year} ${hours}:${minutes}`;
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+  const formattedHours = hours % 12 || 12;
+  return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()} ${formattedHours}:${minutes} ${ampm}`;
 }
